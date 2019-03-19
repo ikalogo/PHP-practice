00e5a97a57bf197e57a2b15705121403ba81d46c
Ikalogo <nakx95@gmail.com>
2019-03-19 16:09:31 +0000

    add comment containing known facts concerning PHP

--- practice-19.php
+++ practice-19.php
@@ -2,4 +2,30 @@
 
 echo "Hello world, it feels soo good to be back!\n";
 
+/**
+ * PHP is the acronym for "Hypertext Preprocessor"
+ * It is an open source scripting language 
+ * Its scripts are executed on the server and results are returned to the browser as HTML
+ * 
+ * PHP can generate dynamic page content
+ * It can create, open, read, write, delete and close files on the server 
+ * It can modify data in your database
+ * It can collect form data
+ * It can encrypt data
+ * 
+ * PHP runs on different platforms
+ * Is compatible with almost all servers used today
+ * Supports a wide range of databases
+ * Is free
+ * 
+ * What's new in PHP 7?
+ * PHP 7 is much faster than the previous stable release (version 5.6)
+ * PHP 7 has improved error handling 
+ * PHP 7 supports stricter Type declaration for functions arguments
+ * PHP 7 supports new operators like the spaceship operator (<=>)
+ * 
+ * In PHP, keywords, classes, function and user defined functions are case-insensitive, while variables are case-sensitive
+ * 
+ */
+
 ?>
