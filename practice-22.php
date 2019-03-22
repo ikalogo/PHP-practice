6d6ab91abbd382f51fbc07a9ef9aeed1d29a1241
Ikalogo <nakx95@gmail.com>
2019-03-22 14:53:22 +0000

    add comment detailing echo vs print in php

---
+++ practice-22.php
@@ -0,0 +1,11 @@
+<?php
+
+//Echo and Print statements 
+
+/**
+ * Both mainly used to print output on the screen. Echo is marginally faster than print.
+ * Print, unlike echo can only accept on argument and has a return value of 1, hence it can be used in expressions while echo has none
+ * The echo statement can be used with or without parentheses: echo, echo()
+ */
+
+?>
